933f82e6f3a4123b3695451f471c30f4e651a61e
Justinnkar <jkarpman01@gmail.com>
2023-06-04 23:38:58 -0700

    JavaScript and Ruby completed.

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -14,46 +14,114 @@
 
 // a) Create a test with an expect statement using the variable provided.
 
-Describe("createSentence", () => 
+describe("createSentence", () => 
 {
   const hitchhikersCharacters = [
     { name: "ford prefect", occupation: "a hitchhiker" },
     { name: "zaphod beeblebrox", occupation: "president of the galaxy" },
     { name: "arthur dent", occupation: "a radio employee" }
   ]
+
 it("takes in an array of objects and returns an array with a sentence about each person with their name capitalized.", () => {
-  expect(createSentence).toEqual["Ford Prefect is a hitchhiker.", "Zaphod Beeblebrox is president of the galaxy.", "Arthur Dent is a radio employee."]
+  expect(createSentence(hitchhikersCharacters)).toEqual(["Ford Prefect is a hitchhiker.", "Zaphod Beeblebrox is president of the galaxy.", "Arthur Dent is a radio employee."])
 })
 })
 
 /*
-FAIL  ./code-challenges.test.js
-  ● Test suite failed to run
+ FAIL  ./code-challenges.test.js
+  createSentence
+    ✕ takes in an array of objects and returns an array with a sentence about each person with their name capitalized. (1 ms)
 
-    ReferenceError: Describe is not defined
+  ● createSentence › takes in an array of objects and returns an array with a sentence about each person with their name capitalized.
+
+    ReferenceError: createSentence is not defined
 */
 
 // b) Create the function that makes the test pass.
 
+/*
+PseudoCode:
+Input:  An array of objects
+Ouput:  An array of strings
+Iterate through each object's name and occupation 
+Capitalize the first letter of the name string
+concatenate the string of "is" between the string of name and the string of occupation
+*/
+
+const createSentence = (array) => {
+    return array.map(object => `${object.name.split(" ").map(name => name.charAt(0).toUpperCase() + name.slice(1)).join(" ")} is ${object.occupation}.`) /* string interpolation begins, iterates through the object, splits the words into an array of substrings, iterates through the substrings capitalizing the first character of each word and adds the remaining part of the word, joins the strings back together. */
+}
 
 // --------------------2) Create a function that takes in a mixed data array and returns an array of only the REMAINDERS of the numbers when divided by 3.
 
 // a) Create a test with an expect statement using the variables provided.
 
-const hodgepodge1 = [23, "Heyyyy!", 45, -10, 0, "Yo", false]
-// Expected output: [ 2, 0, -1, 0 ]
-const hodgepodge2 = [5, "Hola", 43, -34, "greetings", true]
-// Expected output: [ 2, 1, -1 ]
+describe("remainder", () => 
+{
+  const hodgepodge1 = [23, "Heyyyy!", 45, -10, 0, "Yo", false]
+  const hodgepodge2 = [5, "Hola", 43, -34, "greetings", true]
+
+it("takes in a mixed data array and returns an array of only the REMAINDERS of the numbers when divided by 3.", () => {
+  expect(remainder(hodgepodge1)).toEqual([ 2, 0, -1, 0 ])
+  expect(remainder(hodgepodge2)).toEqual([ 2, 1, -1 ])
+})
+})
+
+/*
+ FAIL  ./code-challenges.test.js
+  createSentence
+    ✓ takes in an array of objects and returns an array with a sentence about each person with their name capitalized. (2 ms)
+  remainder
+    ✕ takes in a mixed data array and returns an array of only the REMAINDERS of the numbers when divided by 3.
+
+  ● remainder › takes in a mixed data array and returns an array of only the REMAINDERS of the numbers when divided by 3.
+
+    ReferenceError: remainder is not defined
+*/
 
 // b) Create the function that makes the test pass.
+
+const remainder = (array) => {
+  // iterates over the array and filter out non-numeric values
+  const numbers = array.filter(element => typeof element === 'number')
+  // iterates over the numbers array and computes the remainder of each number when divided by 3
+  const remainders = numbers.map(number => number % 3)
+  // returns the array of remainders
+  return remainders
+}
 
 // --------------------3) Create a function that takes in an array of numbers and returns the sum of all the numbers cubed.
 
 // a) Create a test with an expect statement using the variables provided.
 
-const cubeAndSum1 = [2, 3, 4]
-// Expected output: 99
-const cubeAndSum2 = [0, 5, 10]
-// Expected output: 1125
+describe("sumCubedNumbers", () => 
+{
+  const cubeAndSum1 = [2, 3, 4]
+  const cubeAndSum2 = [0, 5, 10]
+
+it("takes in a mixed data array and returns an array of only the REMAINDERS of the numbers when divided by 3.", () => {
+  expect(sumCubedNumbers(cubeAndSum1)).toEqual(99)
+  expect(sumCubedNumbers(cubeAndSum2)).toEqual(1125)
+})
+})
+
+/*
+ FAIL  ./code-challenges.test.js
+  createSentence
+    ✓ takes in an array of objects and returns an array with a sentence about each person with their name capitalized. (2 ms)
+  remainder
+    ✓ takes in a mixed data array and returns an array of only the REMAINDERS of the numbers when divided by 3. (1 ms)
+  sumOfNumbersCubed
+    ✕ takes in a mixed data array and returns an array of only the REMAINDERS of the numbers when divided by 3.
+
+  ● sumOfNumbersCubed › takes in a mixed data array and returns an array of only the REMAINDERS of the numbers when divided by 3.
+
+    ReferenceError: sumOfNumbersCubed is not defined
+*/
 
 // b) Create the function that makes the test pass.
+
+const sumCubedNumbers = numbers => numbers.reduce((sum, number) => sum + Math.pow(number, 3), 0);
+// reduce calculates the sum of the cubed numbers. 
+
+
--- rails-commenting.rb
+++ rails-commenting.rb
@@ -6,25 +6,25 @@
 
 # FILE: app/controller/blog_posts_controller.rb
 
-# ---1)
+# ---1) A class is defined which serves as a controller to handle blog posts
 class BlogPostsController < ApplicationController
   def index
-    # ---2)
+    # ---2) Index method is defined which retrieves all blog posts from the database. Assigns them to @posts
     @posts = BlogPost.all
   end
 
-  # ---3)
+  # ---3) A method is defined to retrieve specific blog posts based on id.
   def show
     @post = BlogPost.find(params[:id])
   end
 
-  # ---4)
+  # ---4) A method is defined to initialize new BlogPost object and assign it to @post. Used to render a form.
   def new
     @post = BlogPost.new
   end
 
   def create
-    # ---5)
+    # ---5) Method to create a blog post using data from form. If the blog post is valid and successfully created the user is redirected.
     @post = BlogPost.create(blog_post_params)
     if @post.valid?
       redirect_to blog_post_path(@post)
@@ -32,13 +32,13 @@ class BlogPostsController < ApplicationController
   end
 
   def edit
-    # ---6)
+    # ---6) Method used to retrieve exsisting blog post based on id and renders a form to edit exsisting post.
     @post = BlogPost.find(params[:id])
   end
 
   def update
     @post = BlogPost.find(params[:id])
-    # ---7)
+    # ---7) Method to update @post object passing the blog_post_params. If successful the user is redirected to blog_post_path for the updated post.
     @post.update(blog_post_params)
     if @post.valid?
       redirect_to blog_post_path(@post)
@@ -48,15 +48,15 @@ class BlogPostsController < ApplicationController
   def destroy
     @post = BlogPost.find(params[:id])
     if @post.destroy
-      # ---8)
+      # ---8) Method to retrieve and destroy blog post based on id. If successful redirected to the index page to see updated list.
       redirect_to blog_posts_path
     end
   end
 
-  # ---9)
+  # ---9) Private keyword user to indicate methods below are private and can only be accessed withith the scope of the BlogPostsController class.
   private
   def blog_post_params
-    # ---10)
+    # ---10) Encapsulates permitted parameters for updating blog posts. Uses params to retrieve the blog_post parameter, then calls permit to specifiy attributes title and content and allowed to be mass assigned. 
     params.require(:blog_post).permit(:title, :content)
   end
 end
